9ac1daab27bac2672235a80a561c8d3e351cf6fa
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2024-10-10 23:15:52 +0300

    Added Bd controller, module and service to the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BdModule } from './bd/bd.module';
+import { BdController } from './bd/bd.controller';
+import { BdService } from './bd/bd.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [BdModule],
+  controllers: [BdController],
+  providers: [BdService],
 })
 export class AppModule {}
