ad954e7771535e8ef242bcf0643cdc9998f8fb6f
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2024-10-10 19:43:56 +0300

    Add TS types for disk object

---
+++ backend/src/types/diskType.ts
@@ -0,0 +1,15 @@
+
+export type Url = String;
+
+export type Disk = {
+    id: Number,
+    name: String,
+    description: String,
+    janre: String,
+    author: String,
+    picture: Url,
+    price: Number,
+    actionPrice: Number,
+    currency: String,
+    popularityInd: number,
+}
