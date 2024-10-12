6c9b7b22c1760a01289117d18028dd5cc0c3c608
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2024-10-12 16:23:32 +0300

    Added welcome block

---
+++ frontend/src/components/welcomeBlock.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export class WelcomeBlock extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="welcome-block">
+                <div className="welcome-block-image-overlay"></div>
+                <div className="welcome-block-text">
+                    <h1 className="welocme-block-text-line-1">Vinil</h1>
+                    <h1 className="welocme-block-text-line-2">Voyage</h1>
+                    <h2 className="welocme-block-text-line-3">in Minsk</h2>
+                </div>
+            </div>
+        )
+    }
+
+}
